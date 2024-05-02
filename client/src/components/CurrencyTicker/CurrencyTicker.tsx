88337e28ebbc7672442f7cbb654510b5ac824ffa
Roman Lisenkov <rolisoleg@gmail.com>
2024-05-02 23:32:24 +0300

    Added currrency with timer

---
+++ client/src/components/CurrencyTicker/CurrencyTicker.tsx
@@ -0,0 +1,66 @@
+﻿import { useEffect, useState } from 'react';
+import styles from './CurrencyTicker.module.scss';
+import { Hourglass, RotatingLines } from 'react-loader-spinner';
+
+function CurrencyTicker(): JSX.Element {
+  const [price, setPrice] = useState<null | number>(null);
+  const [timer, setTimer] = useState<number>(5);
+
+  const colors = {
+    0: 'text-red-600',
+    1: 'text-red-600',
+    2: 'text-orange-500',
+    3: 'text-orange-500',
+    4: 'text-green-500',
+    5: 'text-green-500',
+  };
+
+  useEffect(() => {
+    const intervalFetch = setInterval(() => {
+      fetch('https://alfabit.org/api/v1/cashe/operations/Tether(USDT)%20TRC20')
+        .then((res) => {
+          setTimer(5);
+          return res.json();
+        })
+        .then((res) => setPrice(res[0].exchange_rate.toFixed(4)))
+        .catch((e) => console.log({ ERROR_GET_CURRENCY: e }));
+    }, 6000);
+    return () => {
+      clearInterval(intervalFetch);
+    };
+  }, []);
+
+  useEffect(() => {
+    const intervalTimer = setInterval(() => {
+      setTimer((prev) => (prev === 0 ? 0 : prev - 1));
+    }, 1000);
+    return () => {
+      clearInterval(intervalTimer);
+    };
+  }, []);
+
+  return (
+    <div className={styles.wrapperCurrencyTicker}>
+      {price ? (
+        <div className={styles.timerCurrencyBlock}>
+          <p>Текущий курс: 1 USDT = {price} RUB</p>
+          <p className={colors[String(timer)]}>0{timer}:00</p>
+        </div>
+      ) : (
+        <div className={styles.timerCurrencyBlock}>
+          <Hourglass
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="hourglass-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            colors={['#cdff00', '#00ffff']}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default CurrencyTicker;
