0676456edf3227f404873bdf269b07ce46681537
Roman Lisenkov <rolisoleg@gmail.com>
2024-05-05 21:42:13 +0300

    Added description in main page and footer

--- client/src/components/MainPage/MainPage.tsx
+++ client/src/components/MainPage/MainPage.tsx
@@ -1,12 +1,16 @@
 ﻿import CurrencyTicker from '../CurrencyTicker/CurrencyTicker';
+import Footer from '../Footer/Footer';
 import FormExchange from '../FormExchange/FormExchange';
 import Header from '../Header/Header';
+import MainDescription from '../MainDescription/MainDescription';
 function MainPage(): JSX.Element {
   return (
     <>
       <Header />
       <CurrencyTicker />
       <FormExchange />
+      <MainDescription/>
+      <Footer/>
     </>
   );
 }
