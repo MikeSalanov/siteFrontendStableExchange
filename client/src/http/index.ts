139a22899e120428859a1734b5917215049ee70b
Halis Basov <has.basov@gmail.com>
2024-05-14 22:02:33 +0300

    uncommented response interceptors

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -14,23 +14,23 @@ $api.interceptors.request.use((config) => {
     return config;
 })
 
-// $api.interceptors.response.use((config) => {
-//     return config;
-// },async (error) => {
-//     const originalRequest = error.config;
-//     console.log(originalRequest);
-//
-//     if (error.response.status == 401 && error.config && !error.config._isRetry) {
-//         originalRequest._isRetry = true;
-//         try {
-//             const response = await axios.get<AuthResponse>(`${AUTH_API_URL}/refresh`, {withCredentials: true})
-//             localStorage.setItem('token', response.data.accessToken);
-//             return $api.request(originalRequest);
-//         } catch (e) {
-//             console.log('НЕ АВТОРИЗОВАН')
-//         }
-//     }
-//     throw error;
-// })
+$api.interceptors.response.use((config) => {
+    return config;
+},async (error) => {
+    const originalRequest = error.config;
+    console.log(originalRequest);
+
+    if (error.response.status == 401 && error.config && !error.config._isRetry) {
+        originalRequest._isRetry = true;
+        try {
+            const response = await axios.get<AuthResponse>(`${AUTH_API_URL}/refresh`, {withCredentials: true})
+            localStorage.setItem('token', response.data.accessToken);
+            return $api.request(originalRequest);
+        } catch (e) {
+            console.log('НЕ АВТОРИЗОВАН')
+        }
+    }
+    throw error;
+})
 
 export default $api;
